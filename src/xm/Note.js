e34d4d86cabba3b1ad0fc75984a95f3c82367afe
Raphael Pigulla <pigulla@four66.com>
2017-11-16 16:50:00 +0100

    Add rudimentary console visualizer

--- src/xm/Note.js
+++ src/xm/Note.js
@@ -9,7 +9,8 @@ const notes = [
     'C-4', 'C#4', 'D-4', 'D#4', 'E-4', 'F-4', 'F#4', 'G-4', 'G#4', 'A-4', 'A#4', 'H-4',
     'C-5', 'C#5', 'D-5', 'D#5', 'E-5', 'F-5', 'F#5', 'G-5', 'G#5', 'A-5', 'A#5', 'H-5',
     'C-6', 'C#6', 'D-6', 'D#6', 'E-6', 'F-6', 'F#6', 'G-6', 'G#6', 'A-6', 'A#6', 'H-6',
-    'C-7', 'C#7', 'D-7', 'D#7', 'E-7', 'F-7', 'F#7', 'G-7', 'G#7', 'A-7', 'A#7', 'H-7'
+    'C-7', 'C#7', 'D-7', 'D#7', 'E-7', 'F-7', 'F#7', 'G-7', 'G#7', 'A-7', 'A#7', 'H-7',
+    ' ▭ '
 ];
 
 function is_compression_info(byte) {
@@ -39,22 +40,28 @@ class Note {
         this.is_compressed = is_compression_info(first_byte);
 
         if (this.is_compressed) {
-            const compression = unpack_compression(first_byte);
             let offset = 1;
+            const {
+                note_follows, instrument_follows, volume_follows, effect_type_follows, effect_parameter_follows
+            } = unpack_compression(first_byte);
 
-            if (compression.note_follows) {
+            if (note_follows) {
                 this.note = buffer.readUInt8(offset++);
             }
-            if (compression.instrument_follows) {
+            if (instrument_follows) {
                 this.instrument = buffer.readUInt8(offset++);
             }
-            if (compression.volume_follows) {
+            if (volume_follows) {
                 this.volume = buffer.readUInt8(offset++);
             }
-            if (compression.effect_type_follows) {
+            if (effect_type_follows) {
                 this.effect_type = buffer.readUInt8(offset++);
+
+                if (!effect_parameter_follows) {
+                    this.effect_parameter = 0;
+                }
             }
-            if (compression.effect_parameter_follows) {
+            if (effect_parameter_follows) {
                 this.effect_parameter = buffer.readUInt8(offset++);
             }
 
@@ -74,59 +81,8 @@ class Note {
         return this.note === 97;
     }
 
-    volume_string() {
-        const v = this.volume;
-
-        if (v === null) {
-            return '   ';
-        } else if (v >= 0x10 && v <= 0x50) {
-            // set volume
-            return ' ' + (v - 0x10).toString(16).padStart(2, '0');
-        } else if (v >= 0x60 && v <= 0x6f) {
-            // volume slide down
-            return '▼' + (v - 0x60).toString(16);
-        } else if (v >= 0x70 && v <= 0x7f) {
-            // volume slide up
-            return '▲' + (v - 0x70).toString(16);
-        } else if (v >= 0x80 && v <= 0x8f) {
-            // fine volume slide down
-            return '▽' + (v - 0x80).toString(16);
-        } else if (v >= 0x90 && v <= 0x9f) {
-            // fine volume slide up
-            return '△' + (v - 0x90).toString(16);
-        } else if (v >= 0xa0 && v <= 0xaf) {
-            // set vibrato speed
-            return '≈' + (v - 0xa0).toString(16);
-        } else if (v >= 0xb0 && v <= 0xbf) {
-            // vibrato
-            return '~' + (v - 0xb0).toString(16);
-        } else if (v >= 0xc0 && v <= 0xcf) {
-            // set panning
-            return '◇' + (v - 0xc0).toString(16);
-        } else if (v >= 0xd0 && v <= 0xdf) {
-            // panning slide left
-            return '◁' + (v - 0xd0).toString(16);
-        } else if (v >= 0xe0 && v <= 0xef) {
-            // panning slide right
-            return '▷' + (v - 0xe0).toString(16);
-        } else if (v >= 0xf0 && v <= 0xff) {
-            // tone porta
-            return '◎' + (v - 0xf0).toString(16);
-        } else {
-            return `??? (${v})`;
-        }
-    }
-
-    toString() {
-        return (this.is_compressed ? '@[' : ' [') + (
-                [
-                    this.note === null ? '   ' : (this.is_key_off() ? ' ▭ ' : (notes[this.note] || '???')),
-                    this.instrument !== null ? this.instrument.toString(16).padStart(2, '0') : '  ',
-                    this.volume_string(),
-                    this.effect_type !== null ? this.effect_type.toString(16).padStart(2, '0') : '  ',
-                    this.effect_parameter !== null ? this.effect_parameter.toString(16).padStart(2, '0') : '  ',
-                ].join(' ')
-            ) + `] (${this.size})`;
+    to_key() {
+        return this.note !== null ? notes[this.note] : null;
     }
 }
 
--- src/xm/Pattern.js
+++ src/xm/Pattern.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const Note = require('./Note');
 
 class Pattern {
@@ -14,6 +15,12 @@ class Pattern {
         this.size = this.data_size + 9;
     }
 
+    notes_for_row(row, num_channels) {
+        assert(isFinite(row) && row >= 0 && row < this.num_rows);
+
+        return this.notes.slice(row * num_channels, row * num_channels + num_channels);
+    }
+
     notes_by_row() {
         const result = (new Array(this.num_rows)).fill(null).map(_ => []);
 
@@ -25,11 +32,11 @@ class Pattern {
         return result;
     }
 
-    notes_by_track(tracks) {
-        const result = (new Array(tracks)).fill(null).map(_ => []);
+    notes_by_channel(num_channels) {
+        const result = (new Array(num_channels)).fill(null).map(_ => []);
 
         for (let i = 0; i < this.notes.length; i++) {
-            result[i % tracks].push(this.notes[i]);
+            result[i % num_channels].push(this.notes[i]);
         }
 
         return result;
